48990f9c5bde1839ff87499b57b23d7f7caa3c57
GOGOYS <qaz75391@naver.com>
2022-03-22 08:16:28 +0900

    HelloKorea

---
+++ HelloJava/src/HelloKorea.java
@@ -0,0 +1,24 @@
+/*
+ * 지금부터 자바코드를 작성한다
+ * 이곳은 주석 영역입니다. (Comment, Remark)영역입니다. 
+ */
+
+//자바의 소스코드는 반드시 public class 클래스명 형식으로 시작된다.
+//클래스명(HelloKorea) 첫글자는 반드시 대문자
+//이후에는 소문자, 숫자, UnderScore, UnderBar의 조합으로 만든다
+
+//Camel Case 명명법
+//두개이상의 단어를 조합하여 만드는 것이 권장되며
+//새로운 단어가 시작되는 곳에서는 대문자로 시작한다
+public class HelloKorea {
+	public static void main(String[] args) {
+		System.out.println("우리나라 만세이~");
+	}
+	// main +컨트롤+스페이스바 자동완성
+	// Sysout +컨트롤 +스페이스바 자동완성
+	
+//	이클립스와 같이 코드를 작성후 컴파일 테스트 완료해서 확인해볼수 있게 만드는 도구
+//	통합개발환경 IDE  Integrated Development Environment
+	
+
+}
---
+++ HelloKorea/src/com/callor/hello/HelloKorea.java
@@ -0,0 +1,14 @@
+package com.callor.hello;
+
+public class HelloKorea {
+
+	public static void main(String[] args) {
+		// main이 없으면 파일자체가 작동을 안함, 필수!!!		
+
+		System.out.println("안녕하세요");
+		// ("")안에 있는 문장을 콘솔에 출력하라는 명령문		
+		// ""안에 글씨를 써야함 ''는 쓰면 에러남
+		System.out.println(30+40);
+	}
+
+}
